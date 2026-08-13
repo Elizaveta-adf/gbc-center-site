a198f50c9705695786c3243f78f29f2f6446cafc
Elizaveta-adf <lissena2006@gmail.com>
2026-08-13 15:56:37 +0700

    Подготовил сайт для Timeweb

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, send_from_directory
+import os
 
 app = Flask(__name__)
 
@@ -6,6 +7,22 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     return render_template("index.html")
+
+
+@app.route("/robots.txt")
+def robots():
+    return send_from_directory(
+        os.path.join(app.root_path, "static"),
+        "robots.txt"
+    )
+
+
+@app.route("/sitemap.xml")
+def sitemap():
+    return send_from_directory(
+        os.path.join(app.root_path, "static"),
+        "sitemap.xml"
+    )
 
 
 if __name__ == "__main__":
